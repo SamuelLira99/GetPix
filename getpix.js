671255a9ce2c9aa29ecd036644854e6fbde9241b
SamuelLira <samuellira99@protonmail.com>
2021-02-13 18:18:56 -0300

    A busca agora é feita utilizando regex

--- getpix.js
+++ getpix.js
@@ -3,13 +3,17 @@ let copyPaste = prompt('Pix Copia e Cola:');
 // chaves aleatórias possuem 36 dígitos (incluindo '-' (traços))
 const keyLength = 36;
 
-// usado para determinar o início da chave pix (8 dígidos antes do primeiro traço)
-let pos = copyPaste.indexOf('-');
-let keyFirstCharPos = pos - 8;
+// determina o início da chave pix utilizando regex
+let keyFirstCharPos = copyPaste.search(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/);
 
-// extrai a chave de [8 dígitos antes do 1° traço] até [36 dígitos após o primeiro]
-let key = copyPaste.substr(keyFirstCharPos, keyLength);
+// verifica se a chave foi encontrada
+if(keyFirstCharPos < 0) {
+    alert('O código informado não contém uma chave pix aleatória')
+} else {
 
+    // extrai a chave de [posição encontrada pelo regex] até [36 dígitos após a posição]
+    let key = copyPaste.substr(keyFirstCharPos, keyLength);
 
-console.log(key);
-alert('Sua chave Pix:\n\n'+key);
+    console.log(key);
+    alert('Sua chave Pix:\n\n'+key);
+}
